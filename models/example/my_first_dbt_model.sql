76917c810aa0b160200ccf085be41029c4116a47
pink1124 <ranji.gandhasiri123@gmail.com>
2021-11-19 17:14:09 +0530

    Update my_first_dbt_model.sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,3 +1,4 @@
+
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -7,7 +8,17 @@
 
 {{ config(materialized='table') }}
 
-select location, population from COVID_19_INDONESIA_RANJITH_GANDHASIRI
+with source_data as (
+
+    select 1 as id
+    union all
+    select null as id
+
+)
+
+select *
+from source_data
+where id is not null
 
 /*
     Uncomment the line below to remove records with null `id` values
